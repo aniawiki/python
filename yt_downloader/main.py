b3d5f5845731d541a9688070a5e07d0d93c5328a
mzums <annsie035@gmail.com>
2023-09-09 20:54:24 +0200

    download mp3 or mp4

--- yt_downloader/main.py
+++ yt_downloader/main.py
@@ -1,18 +1,24 @@
 import pytube
-
+from pydub import AudioSegment
+import os
 
 link = input("Link to the video: ")
 yt = pytube.YouTube(link)
 
 valid_format = False
 
-while (not (valid_format)):
-    format = input("mp4 or 3gpp? ")
-    if (format == "mp4"):
-        stream = yt.streams.filter(file_extension='mp4').first()
-        valid_format = True
-    elif (format == "3gpp"):
-        stream = yt.streams.first()
+while not valid_format:
+    format = input("Choose the format (mp4 or mp3): ")
+    stream = yt.streams.filter(file_extension='mp4').first()
+    if (format == "mp4" or format == "mp3"):
         valid_format = True
 
-stream.download()
+video_path = stream.download()
+
+file_name = input("Enter the desired file name (without extension): ")
+
+audio = AudioSegment.from_file(video_path, format="mp4")
+audio.export(f"{file_name}.{format}", format=format)
+
+os.remove(video_path)
+print(f"File {file_name}.{format} has been saved.")
