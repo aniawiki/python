8c03e8c12ef33c86ef0520e0d4fe0a7442e62fbe
mzums <annsie035@gmail.com>
2023-09-25 19:40:54 +0200

    turtle binary tree

---
+++ binary_tree/main.py
@@ -0,0 +1,33 @@
+import turtle
+
+i = 50
+
+tu = turtle.Turtle()
+tu.screen.bgcolor("black")
+tu.pensize(2)
+tu.color("brown")
+
+tu.left(90)
+tu.backward(i)
+tu.speed(200)
+
+
+def tree(i):
+    if i < 10:
+        return
+    else:
+        tu.forward(i)
+        tu.color("orange")
+        tu.circle(2)
+        tu.color("brown")
+
+        tu.left(30)
+        tree(3*i/4)
+        tu.right(60)
+        tree(3*i/4)
+        tu.left(30)
+        tu.backward(i)
+
+
+tree(i)
+turtle.done()
